0fbbf01e7035564a5a016926ea6a81d17bcf0466
André Angeloni <andrefangeloni@gmail.com>
2022-04-13 17:08:38 -0300

    feat: styled messagelist

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,1 +1,59 @@
-export const MessageList = () => <h1>MessageList</h1>;
+import logo from '../../assets/logo.svg';
+
+import styles from './styles.module.scss';
+
+export const MessageList = () => (
+  <div className={styles.messageListWrapper}>
+    <img src={logo} alt="DoWhile 2021" />
+
+    <ul className={styles.messageList}>
+      <li className={styles.message}>
+        <p className={styles.messageContent}>
+          Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+          de todos os tempos.
+        </p>
+        <div className={styles.messageUser}>
+          <div className={styles.userImage}>
+            <img
+              src="https://github.com/andrefangeloni.png"
+              alt="André Angeloni"
+            />
+          </div>
+          <span>André Angeloni</span>
+        </div>
+      </li>
+
+      <li className={styles.message}>
+        <p className={styles.messageContent}>
+          Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+          de todos os tempos.
+        </p>
+        <div className={styles.messageUser}>
+          <div className={styles.userImage}>
+            <img
+              src="https://github.com/andrefangeloni.png"
+              alt="André Angeloni"
+            />
+          </div>
+          <span>André Angeloni</span>
+        </div>
+      </li>
+
+      <li className={styles.message}>
+        <p className={styles.messageContent}>
+          Não vejo a hora de começar esse evento, com certeza vai ser o melhor
+          de todos os tempos.
+        </p>
+        <div className={styles.messageUser}>
+          <div className={styles.userImage}>
+            <img
+              src="https://github.com/andrefangeloni.png"
+              alt="André Angeloni"
+            />
+          </div>
+          <span>André Angeloni</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+);
