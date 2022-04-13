e21486e2e15db34afd96888fd01579071e8c57f7
André Angeloni <andrefangeloni@gmail.com>
2022-04-13 19:38:00 -0300

    feat: get messages from API

--- frontend/src/components/MessageList/index.tsx
+++ frontend/src/components/MessageList/index.tsx
@@ -1,59 +1,48 @@
+import React from 'react';
+
 import logo from '../../assets/logo.svg';
+import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
 
-export const MessageList = () => (
-  <div className={styles.messageListWrapper}>
-    <img src={logo} alt="DoWhile 2021" />
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+};
 
-    <ul className={styles.messageList}>
-      <li className={styles.message}>
-        <p className={styles.messageContent}>
-          Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-          de todos os tempos.
-        </p>
-        <div className={styles.messageUser}>
-          <div className={styles.userImage}>
-            <img
-              src="https://github.com/andrefangeloni.png"
-              alt="André Angeloni"
-            />
-          </div>
-          <span>André Angeloni</span>
-        </div>
-      </li>
+export const MessageList = () => {
+  const [messages, setMessages] = React.useState<Message[]>([]);
 
-      <li className={styles.message}>
-        <p className={styles.messageContent}>
-          Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-          de todos os tempos.
-        </p>
-        <div className={styles.messageUser}>
-          <div className={styles.userImage}>
-            <img
-              src="https://github.com/andrefangeloni.png"
-              alt="André Angeloni"
-            />
-          </div>
-          <span>André Angeloni</span>
-        </div>
-      </li>
+  React.useEffect(() => {
+    const getMessages = async () => {
+      const { data } = await api.get<Message[]>('/messages/last3');
+      setMessages(data);
+    };
 
-      <li className={styles.message}>
-        <p className={styles.messageContent}>
-          Não vejo a hora de começar esse evento, com certeza vai ser o melhor
-          de todos os tempos.
-        </p>
-        <div className={styles.messageUser}>
-          <div className={styles.userImage}>
-            <img
-              src="https://github.com/andrefangeloni.png"
-              alt="André Angeloni"
-            />
-          </div>
-          <span>André Angeloni</span>
-        </div>
-      </li>
-    </ul>
-  </div>
-);
+    getMessages();
+  }, []);
+
+  return (
+    <div className={styles.messageListWrapper}>
+      <img src={logo} alt="DoWhile 2021" />
+
+      <ul className={styles.messageList}>
+        {messages.map((message) => (
+          <li key={message.id} className={styles.message}>
+            <p className={styles.messageContent}>{message.text}</p>
+            <div className={styles.messageUser}>
+              <div className={styles.userImage}>
+                <img src={message.user.avatar_url} alt={message.user.name} />
+              </div>
+              <span>{message.user.name}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
