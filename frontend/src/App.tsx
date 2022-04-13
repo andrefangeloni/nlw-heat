ab5bf9cbdd4e635d6822b561ac9d49d0620e8c4a
André Angeloni <andrefangeloni@gmail.com>
2022-04-12 22:05:03 -0300

    feat: environment setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,7 @@
-export const App = () => <h1>Hello World!</h1>;
+import styles from './App.module.scss';
+
+export const App = () => (
+  <div className={styles.contentWrapper}>
+    <h1>Hello World!</h1>
+  </div>
+);
